48d4657e20f490cf14a494ed6da1f3465cbf0d80
StrawEater <59102276+StrawEater@users.noreply.github.com>
2021-08-27 13:08:03 -0300

    Reverting the change

--- SpinningSquare.js
+++ SpinningSquare.js
@@ -78,7 +78,7 @@ class SpinningSquare {
       }
     }
 
-    if (wholeSize > 0) {
+    if (segment1 - wholeSize * segment2 > 2) {
       if (horizontal) {
         start[0] += wholeSize * segment2;
 
